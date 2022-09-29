3efcb162979f48261cb47e4e46e67b0c138b9cfc
pratham4git <prathameshmitawalkar@gmail.com>
2022-09-29 16:56:00 +0530

    with dummy json data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [item, setItem ] = useState({ firstName: '', lastName: '', age: ''}); 
   const [itemList, setItemList] = useState([]);
+
+  useEffect(
+    ()=> {
+      const fetchData = async () => {
+        const response = await fetch("https://dummyjson.com/users/1");
+        const data = await response.json();        
+        setItemList([...itemList, data]);        
+      }
+      fetchData();      
+    }, []
+  )
+
+   
 
   return (
     <div className="App">
