5133779fe947931104747f50871587d44aeea169
pratham4git <prathameshmitawalkar@gmail.com>
2022-09-29 15:39:47 +0530

    First commit for MERN Client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [item, setItem ] = useState({ firstName: '', lastName: '', age: ''}); 
+  const [itemList, setItemList] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='flx'>   
+      <form>
+        <label>Enter First Name:</label>
+        <input
+            value={item.firstName}
+            onChange={(e)=>setItem({...item, firstName: e.target.value})}
+            type='text'
+        />
+        <label>Enter Last Name:</label>
+        <input
+          value={item.lastName}
+          onChange={(e)=> setItem({...item, lastName: e.target.value})}
+        />
+        <label>Enter Age:</label>
+        <input
+          value={item.age}
+          onChange={(e)=>setItem({...item, age: e.target.value})}
+        />
+        <button className='btn' onClick={(e)=>{
+          e.preventDefault();
+          setItemList( (prevList) => {
+            return [...itemList, item]
+          }            
+          ); 
+          setItem({firstName: '', lastName: '', age: '' });       
+      }}>Submit</button>
+        {console.log(item.firstName, item.lastName, item.age)}
+        {console.log(itemList)}
+      </form>
+
+      <ul>
+        {itemList.map((i)=>(<li key={i.firstName}>{i.firstName} {i.lastName} {i.age}
+        <button className='btn' onClick={()=> setItemList(itemList.filter((it)=> it.firstName !== i.firstName))}>Delete</button>
+         </li>))}
+      </ul>
+      </div>   
+    </div>    
   );
 }
 
